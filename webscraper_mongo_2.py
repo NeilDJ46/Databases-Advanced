f499db2481df669fb500df694292217e0661e011
NeilDJ46 <71898258+NeilDJ46@users.noreply.github.com>
2021-05-08 14:33:49 +0200

    Update webscraper_mongo_2.py

--- webscraper_mongo_2.py
+++ webscraper_mongo_2.py
@@ -17,13 +17,13 @@ client = redis.Redis(host = '127.0.0.1', port = 6379)
 #Function that we will loop over every minute
 def scraper():
     records = client.get("HashValues")
-
+    print(records)
     #Sort the records list by the value of the Bitcoin which sits at position 2 of the list
-    records.sort(key=lambda x:x[2])
+    #records.sort(key=lambda x:x[2])
 
     #Create a JSON object (post) from our highest value Hash and insert it into our collection "HashValues"
-    post = {"Hash": records[-1][0], "Time": records[-1][1], "BTC": records[-1][2], "USD": records[-1][3]}
-    collection.insert_one(post)
+    #post = {"Hash": records[-1][0], "Time": records[-1][1], "BTC": records[-1][2], "USD": records[-1][3]}
+    #collection.insert_one(post)
 
     #Confirmation our data is added to our collection
     print("Added to collection")
